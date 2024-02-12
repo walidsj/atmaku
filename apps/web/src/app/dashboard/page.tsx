8cb97067797c479a62a1c53f9a97ee347d5d9d98
Moh. Walid Arkham Sani <mohwalidas@outlook.com>
2024-02-13 07:12:40 +0800

    refactor: clear dashboard

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,19 +1,14 @@
 import DashboardLayout from '@/components/dashboard-layout'
 import { Card, CardContent } from '@/components/ui/card'
-import useAuth from '@/lib/auth'
 
 export default function Dashboard() {
-   const { user } = useAuth()
-
    return (
       <DashboardLayout
          title="Dashboard"
          description="Ringkasan data dan statistik kinerja keuangan BLUD"
       >
          <Card>
-            <CardContent className="p-5">
-               {user && <div>Welcome, {user.nama}</div>}
-            </CardContent>
+            <CardContent className="p-5">Welcome</CardContent>
          </Card>
       </DashboardLayout>
    )
